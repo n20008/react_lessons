6e8dc3e45ad97f0976571ad8e5e0bc0180443af6
n20008 <n20008@std.it-college.ac.jp>
2021-02-03 05:28:56 +0000

    go

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className='wether'>
+        <h1>天気予報</h1>
+      </div>
+    )
+  }
+}
+
+export default App

23b5c760a704bcdd54ae2e8d115c3193cb62bfbb
n20008 <n20008@std.it-college.ac.jp>
2021-02-08 02:20:48 +0000

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,75 @@
 import React from 'react'
+import axios from 'axios'
 import './App.css'
 
-class App extends React.Component {
+const API_ENDPOINT = 'http://api.openweathermap.org/data/2.5/forecast'
 
-  
+export default class extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      apiKey: 'bfa294572295d97adca32fc157ad8185',
+      requestCity: '',
+      city: '',
+      response: []
+    }
+    this.handleInput = this.handleInput.bind(this)
+    this.handleGetWeather = this.handleGetWeather.bind(this)
+  }
+
+  handleGetWeather () {
+    axios
+      .get(API_ENDPOINT, {
+        params: {
+          q: this.state.requestCity,
+          APPID: this.state.apiKey
+        }
+      })
+      .then(res => {
+        this.setState({
+          response: res.data.list,
+          city: res.data.city.name
+        })
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+
+  handleInput ({ target: { value } }) {
+    this.setState({
+      requestCity: value
+    })
+  }
+
   render () {
+    console.log(this.state.response)
+
     return (
-      <div className='wether'>
-        <h1>天気予報</h1>
+      <div className='weather'>
+        <h1>天気</h1>
+        <input
+          type='text'
+          value={this.state.requestCity}
+          onChange={this.handleInput}
+        />
+        <button onClick={this.handleGetWeather}>Search</button>
+        <p> Location: {this.state.city} </p>
+        {Object.keys(this.state.response).map(key => (
+          <li key={key}>
+            {this.state.response[key].dt_txt}
+            ,
+            <img
+              src={
+                'http://openweathermap.org/img/w/' +
+                this.state.response[key].weather[0].icon +
+                '.png'
+              }
+            />
+            {this.state.response[key].weather[0].main}
+          </li>
+        ))}
       </div>
     )
   }
 }
-
-export default App
